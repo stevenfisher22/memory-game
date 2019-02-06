6e8a3bc32f5636bd076f60db13872c4d0e25a6b9
Steven Fisher <steven.fisher22@gmail.com>
2019-02-06 13:35:42 -0600

    added single card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import MemoryCard from './MemoryCard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Memory Game
-          </a>
+          <h1>Memory Game</h1> 
+          <h2 id="subtitle">Match Cards to Win</h2>
         </header>
+        <MemoryCard />
       </div>
     );
   }
---
+++ src/MemoryCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './MemoryCard.css'
+
+class MemoryCard extends Component {
+    render() {
+        return (
+            <div class="memoryCard">
+                <img class="card" src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" alt=""></img>
+            </div>
+        )
+    }
+}
+
+
+
+export default MemoryCard;
