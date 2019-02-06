16f1afd0e547bcbd707c1c32d066da65553dd69f
Steven Fisher <steven.fisher22@gmail.com>
2019-02-06 14:52:50 -0600

    cards are flippable

--- src/MemoryCard.js
+++ src/MemoryCard.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 import './MemoryCard.css'
 
 class MemoryCard extends Component {
-    // clickHandler(() => {
-    //     alert("Card Clicked")
-    // })
+    constructor() {
+        super();
+        this.state = {isFlipped: false};
+    }
+
+    clickHandler() {
+        this.setState({
+            isFlipped: !this.state.isFlipped
+        });
+    }
 
     render() {
+        let memoryCardInnerClass = 'memoryCardInner';
+        if (this.state.isFlipped) {
+            memoryCardInnerClass += " flipped";
+        }
+
         return (
-            <div class="memoryCard" onClick={this.clickHandler}>
-                <div class="memoryCardInner">
-                    <div class="memoryCardBack">
+            <div className="memoryCard" onClick={this.clickHandler.bind(this)}>
+                <div className={memoryCardInnerClass}>
+                    <div className="memoryCardBack">
                         <img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" alt=""></img>
                     </div>
-                    <div class="memoryCardFront">
-                        ∆
-                    </div>
+                    <div className="memoryCardFront">∆</div>
                 </div>
             </div>
         )
