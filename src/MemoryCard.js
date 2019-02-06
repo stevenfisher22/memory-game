85b6d53fa96a57ad742ae4a993536e70ed167d29
Steven Fisher <steven.fisher22@gmail.com>
2019-02-06 14:08:22 -0600

    front and back cards rendered

--- src/MemoryCard.js
+++ src/MemoryCard.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import './MemoryCard.css'
 
 class MemoryCard extends Component {
+    // clickHandler(() => {
+    //     alert("Card Clicked")
+    // })
+
     render() {
         return (
-            <div class="memoryCard">
-                <img class="card" src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" alt=""></img>
+            <div class="memoryCard" onClick={this.clickHandler}>
+                <div class="memoryCardInner">
+                    <div class="memoryCardBack">
+                        <img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" alt=""></img>
+                    </div>
+                    <div class="memoryCardFront">
+                        ∆
+                    </div>
+                </div>
             </div>
         )
     }
