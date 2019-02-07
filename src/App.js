b422eb2716b45f93105e479db4a9f3234358f854
Steven Fisher <steven.fisher22@gmail.com>
2019-02-06 18:21:23 -0600

    part 3, step 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,41 @@ import React, { Component } from 'react';
 import './App.css';
 import MemoryCard from './MemoryCard';
 
+// Shuffle
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+function generateDeck() {
+  var symbols = ["∆", "ß", "£", "§", "•", "$", "+", "ø"];
+  var deck = [];
+  for (let i = 0; i < 16; i++) {
+    deck.push({
+      symbol: symbols[i%8],
+      isFlipped: false
+    });
+  }
+  shuffle(deck);
+  return deck;
+}
+
+
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      deck: generateDeck(),
+      pickedCards: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
